aa7565f5531902f10f6473cda231b99eb8ffa7c1
Sandile909 <83961798+Sandile909@users.noreply.github.com>
2025-03-15 17:53:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const fs = require('fs');
-const nodemailer = require('nodemailer');
+//const nodemailer = require('nodemailer');
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
